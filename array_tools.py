7657145efd7829b9b203e0f328af36aaa6a63b1f
Trip Hook <jchook3@gmail.com>
2015-03-24 15:57:03 -0400

    Significant changes

--- array_tools.py
+++ array_tools.py
@@ -1,5 +1,3 @@
-__author__ = 'Trip Hook'
-
 import numpy as np
 import gdal, ogr
 import sys
@@ -7,7 +5,7 @@ import collections
 import os
 import csv
 
-# Object representing a simple bounding rectangle
+# Object representing a simple bounding rectangle, used primarily to measure raster overlap
 class Envelope(object):
     def __init__(self, left, right, bottom, top):
         self.left = left
@@ -15,6 +13,7 @@ class Envelope(object):
         self.bottom = bottom
         self.top = top
 
+    #  Returns the rectangle corresponding to the overlap with another Envelope object
     def overlap(self, r2):
         def range_overlap(a_min, a_max, b_min, b_max):
             return (a_min <= b_max) and (b_min <= a_max)
@@ -37,6 +36,78 @@ class Envelope(object):
         if (self.left, self.right, self.bottom, self.top) == (other.left, other.right, other.bottom, other.top):
             return True
         return False
+
+
+# A modified dictionary which holds a set of "reach" objects representing NHD Plus stream reaches
+# indexed by COMID or another user designated "id_field"
+class Hierarchy(dict):
+
+    def __init__(self, region_dir, region,
+                 init_table='PlusFlow', from_field="FROMCOMID", to_field="TOCOMID", id_field="ComID",
+                 exclusions=[("PlusFlowlineVAA", "Fcode", 56600)],
+                 velocity_table='EROM_Annual', velocity_field='V0001E'):  # (table, field, values)
+
+        self.region = region
+        self.dir = region_dir
+        self.from_field = from_field
+        self.to_field = to_field
+        self.id_field = id_field
+        self.velocity_table = velocity_table
+        self.velocity_field = velocity_field
+        self.init_table = init_table
+        self.exclusions = set()
+
+        # Initialize paths to NHD Plus files
+        nhd_files = [('CatchmentRaster', r"NHDPlusCatchment\cat"),
+                     ('CatchmentTable', r"NHDPlusCatchment\featureidgridcode.dbf"),
+                     ('PlusFlowlineVAA', r"NHDPlusAttributes\PlusFlowlineVAA.dbf"),
+                     ('Flowline', r"NHDSnapshot\Hydrography\NHDFlowline.shp"),
+                     ('PlusFlow', r"NHDPlusAttributes\PlusFlow.dbf"),
+                     ('EROM_Annual', r"EROMExtension\EROM_MA0001.dbf")]
+        nhd_files += [('EROM_{}'.format(m), r"EROMExtension\EROM_MA000{}.dbf".format(m)) for m in range(1, 13)]
+        self.paths = collections.defaultdict(lambda: "No path specified")
+        not_found = []
+        for name, path in nhd_files:
+            full_path = os.path.join(self.dir, path)
+            if os.path.exists(full_path):
+                self.paths[name] = full_path
+            else:
+                not_found.append(name)
+        if not_found:
+            print "Files not found for: " + ", ".join(not_found)
+
+        self.initialize()
+        self.exclude(exclusions)
+
+
+    def __missing__(self, key):
+        self[key] = Reach()
+        return self[key]
+
+    def initialize(self):
+        # Read length and velocity attributes into dictionaries
+        length_dict = dict(read_dbf(self.paths['PlusFlowlineVAA'], [self.id_field, "LengthKM"]))
+
+        velocity_dict = {}
+        if self.velocity_table:
+            velocity_dict = dict(read_dbf(self.paths[self.velocity_table], [self.id_field, self.velocity_field]))
+        for reach_id, parent in read_dbf(self.paths[self.init_table], [self.from_field, self.to_field]):
+            if all((reach_id, parent, not reach_id in self.exclusions, not parent in self.exclusions)):
+                self[reach_id].name = reach_id
+                self[parent].name = parent
+                self[reach_id].parent = self[parent]
+                self[parent].children.add(self[reach_id])
+                self[reach_id].length = length_dict.get(reach_id, 0) * 1000  # meters
+                self[reach_id].velocity = velocity_dict.get(reach_id, -1) * 0.3048  # meters per second
+
+    def exclude(self, exclusions):
+        remove_lines = []
+        for table, field, value in sorted(exclusions):
+            params = dict(read_dbf(self.paths[table], [self.id_field, field]))
+            remove_lines += filter(lambda x: params.get(x) == value, self.keys())
+
+        for line in remove_lines:
+            del self[line]
 
 
 # Wrapper for an ESRI raster grid and function for reading into an array
@@ -74,75 +145,68 @@ class Raster(object):
 
 # Object representing a stream reach.  Used to construct a hierarchy for accumulation function
 class Reach(object):
-    def __init__(self):
-        self.name = None
-        self.parent = None
-        self.children = set()
-        self._upstream = set()
-        self._paths = []
+        def __init__(self):
+            self.name = None
+            self.parent = None
+            self.children = set()
+            self._upstream = set()
+            self._paths = []
 
-        self.length = 0  # meters
-        self.velocity = 0  # meters
-        self._time = -1  # seconds
+            self.length = 0  # meters
+            self.velocity = 0  # meters
+            self._time = -1  # seconds
 
-    def __repr__(self):
-        return "Reach({})".format(self.name)
+        def __repr__(self):
+            return "Reach({})".format(self.name)
 
-    @property
-    def time(self):
-        if self._time != -1:
-            return self._time
-        else:
-            if self.length and self.velocity:
+        # Recursively search upstream in the drainage network, returns a set of all upstream reaches
+        @property
+        def all_upstream(self):
+            if not self._upstream:
+                map(lambda x: self._upstream.update(x.all_upstream | {x}), self.children)
+                for child in self.children:
+                    self._upstream.add(child)
+                    self._upstream |= child.all_upstream
+            return self._upstream
+
+        @property
+        def time(self):
+            if self._time == -1 and all((self.length, self.velocity)):
                 self._time = self.length / self.velocity
-            else:
-                self._time = 0
             return self._time
 
-    # Recursively search upstream in the drainage network, returns a set of all upstream reaches
-    @property
-    def upstream(self):
-        if self._upstream:
-            return self._upstream
-        else:
-            for child in self.children:
-                self._upstream.add(child)
-                self._upstream |= child.upstream
-            return self._upstream
-
-    # Recursively search upstream in the drainage network, returns a set of all paths upstream
-    def upstream_paths(self, h):
-        def look_upstream(n):
-            if not h[n].children:
-                self._paths.append(current_path[:])
-            for child in h[n].children:
-                current_path.append(child)
-                look_upstream(child.name)
-            if current_path:
-                current_path.pop()
-        if self._paths:
+        # Recursively search upstream in the drainage network, returns a set of all paths upstream
+        @property
+        def upstream_paths(self):
+            if not self._paths:
+                for child in self.children:
+                    if child.upstream_paths:
+                        self._paths.extend([child] + path for path in child.upstream_paths)
+                    else:
+                        self._paths.append([child])
             return self._paths
-        else:
-            current_path = [self]
-            look_upstream(self.name)
-            return self._paths
-
-    def time_maps(self, h):
-        paths = self.upstream_paths(h)
-        time_maps = [zip(path, np.cumsum([reach.time for reach in path])) for path in paths]
-        items = {s for time_map in time_maps for s in time_map}
-        return items
 
 
 # Accumulates class areas for all reaches upstream of each reach
-def accumulate(allocated_data, flow_table, translate=None, excl_params=None, excl_field=None, excl_vals=None):
+def accumulate(allocated_data, hierarchy, translate=True):
     sys.setrecursionlimit(50000)
-    reaches = build_hierarchy(flow_table, excl_params, excl_field, excl_vals, translate)
-    accumulated = collections.defaultdict(dict)
-    for reach in iter(reaches):
-        us_reaches = set([reach.name] + [r.name for r in reach.upstream])
-        accumulated[reach.name] = sum([allocated_data.get(r, 0.0) for r in us_reaches])
-    return accumulated
+
+    # Translate from GRIDCODE to COMID for NHD Plus catchment allocations
+    if translate:
+        translation_dict = dict(read_dbf(hierarchy.paths['CatchmentTable'], ["GRIDCODE", "FEATUREID"]))
+        translated_comids = map(lambda x: translation_dict.get(x, -1), allocated_data[0])
+        allocated_data = np.vstack([translated_comids, allocated_data[1:]])
+
+    # Accumulate data
+    accumulated_data = np.empty([1, 3])  # Initialize blank array
+    all_classes = np.unique(allocated_data[1])  # Get a set of all unique subclasses
+    for reach_id, reach in hierarchy.iteritems():  # Iterate through all reaches in hierarchy
+        us_reaches = np.array(list(set([reach_id] + [r.name for r in reach.all_upstream])))
+        for _class in all_classes:
+            in_class = allocated_data[:,(allocated_data[1] == _class)]
+            upstream = in_class[2:, np.in1d(in_class[0], us_reaches)]
+            accumulated_data = np.vstack((accumulated_data, np.array([reach_id, _class, upstream.sum()])))
+    return accumulated_data.T
 
 
 # Allocates raster classes to a set of overlapping zones
@@ -183,45 +247,31 @@ def allocate(allocation_raster, zone_rasters, tile='max'):
     return finished
 
 
-def build_hierarchy(flow_table, from_field="FROMCOMID", to_field="TOCOMID", id_field="COMID",
-                    excl_params=None, excl_field=None, excl_vals=None, translate=None):
-
-    # Read from/to relationships in flow table
-    flows = read_dbf(flow_table, [from_field, to_field])
-
-    # Exclude flows that meet certain criteria (e.g., exclude streams where FCODE is 56600 (coastline)
-    if all((excl_params, excl_field, excl_vals)):
-        params = read_dbf(excl_params, [id_field, excl_field])
-        # This bit is necessary because some NHD plus regions have different capitalization patterns
-        if not params:
-            params = dict(read_dbf(excl_params, ['ComID', "FCode"]))
-        params = dict(params)
-        flows = [flow for flow in flows if not set(excl_vals) & set(map(lambda x: params.get(x), flow))]
-
-    # Translate COMID into another field for processing (usually GRIDCODE for raster catchments)
-    if translate:
-        flows = [map(lambda x: translate.get(x), row) for row in flows]
-
-    # Build hierarchy
-    hierarchy = collections.defaultdict(lambda: Reach())
-    for reach_id, parent in flows:
-        if reach_id:
-            hierarchy[reach_id].name = reach_id
-            hierarchy[parent].name = parent
-            hierarchy[reach_id].parent = hierarchy[parent]
-            hierarchy[parent].children.add(hierarchy[reach_id])
-
-    return filter(lambda reach: bool(reach.name), hierarchy.values())
+# Assembles a dictionary of NHD Plus directory structure indexed by region
+def get_nhd(nhd_dir, region_filter='all'):
+    # Get catchment grid, gridcode to comid translation files, and flow tables
+    regions = {'01', '02', '03N', '03S', '03W', '04', '05', '06', '07', '08', '09',
+               '10U', '10L', '11', '12', '13', '14', '15', '16', '17', '18'}
+    all_paths = collections.defaultdict()
+    region_dirs = {"NHDPlus{}".format(region) for region in regions}
+    for root_dir, sub_dirs, _ in os.walk(nhd_dir):
+        if set(sub_dirs) & region_dirs:
+            for sub_dir in sub_dirs:
+                all_paths[sub_dir.lstrip("NHDPlus")] = os.path.join(root_dir, sub_dir)
+    return all_paths
 
 
-# Divides a bounding envelope into smaller tiles for processing on less powerful computers
-def make_tiles(envelope, tile_size):
-    if tile_size == 'max':
-        return [envelope]
-    else:
-        h = range(int(envelope.left), int(envelope.right), tile_size) + [envelope.right]
-        v = range(int(envelope.bottom), int(envelope.top), tile_size) + [envelope.top]
-        return [Envelope(h[i], h[i + 1], v[j], v[j + 1]) for i in range(len(h) - 1) for j in range(len(v) - 1)]
+# Breaks down an allocation array into a nested dictionary
+def make_histogram(output_array):
+    output_dict = nested_dict()
+    for row in output_array.T:
+        row = list(row)
+        top = row.pop(0)
+        active_d = row.pop()
+        for i in range(len(row)):
+            active_d = {row.pop(): active_d}
+        output_dict[top].update(active_d)
+    return output_dict
 
 
 # Returns the average of a sequence
@@ -237,6 +287,16 @@ def nested_dict():
     return collections.defaultdict(nested_dict)
 
 
+# Divides a bounding envelope into smaller tiles for processing on less powerful computers
+def make_tiles(envelope, tile_size):
+    if tile_size == 'max':
+        return [envelope]
+    else:
+        h = range(int(envelope.left), int(envelope.right), tile_size) + [envelope.right]
+        v = range(int(envelope.bottom), int(envelope.top), tile_size) + [envelope.top]
+        return [Envelope(h[i], h[i + 1], v[j], v[j + 1]) for i in range(len(h) - 1) for j in range(len(v) - 1)]
+
+
 # Reads the contents of a dbf table
 def read_dbf(dbf_file, out_fields=None):
     # Initialize file
@@ -249,10 +309,15 @@ def read_dbf(dbf_file, out_fields=None):
     fields = {ld.GetFieldDefn(i).GetName() for i in range(ld.GetFieldCount())}
     if out_fields:
         missing_fields = set(out_fields) - fields
+        remedies = [lambda x: x.upper(), lambda x: x.capitalize()]
+        while missing_fields and remedies:
+            new_fields = map(remedies.pop(0), out_fields)
+            out_fields = [nf if nf in fields else out_fields[i] for i, nf in enumerate(new_fields)]
+            missing_fields = set(out_fields) - fields
         if missing_fields:
-            print "Specified field(s) {} not found in dbf file {}".format(",".join(missing_fields), dbf_file)
-            return None
+            print "Fields {} not found in {}".format(out_fields, dbf_file)
         fields = [field for field in out_fields if not field in missing_fields]
+
     # Read data
     if len(fields) > 1:
         table = [[row.GetField(f) for f in fields] for row in layer]
@@ -261,30 +326,6 @@ def read_dbf(dbf_file, out_fields=None):
     # noinspection PyUnusedLocal
     data_source = None
     return table
-
-
-# Reads a dbf file and returns dictionary with one field as a key and another as a value
-def read_translation(tf, from_field, to_field, flip=False):
-    tt = read_dbf(tf, [from_field, to_field])
-    comid_to_gridcode = dict(tt)
-    if flip:
-        gridcode_to_comid = dict([(row[1], row[0]) for row in tt])
-        return comid_to_gridcode, gridcode_to_comid
-    else:
-        return comid_to_gridcode
-
-
-# Breaks down an allocation array into a nested dictionary
-def make_histogram(output_array):
-    output_dict = nested_dict()
-    for row in output_array.T:
-        row = list(row)
-        top = row.pop(0)
-        active_d = row.pop()
-        for i in range(len(row)):
-            active_d = {row.pop(): active_d}
-        output_dict[top].update(active_d)
-    return output_dict
 
 
 # Sums all values for each group
@@ -305,7 +346,7 @@ def sum_by_group(groups, values, oper='sum'):
 
 
 # Write dictionary or NumPy array to csv file
-def write_to_file(output_dict, outfile_name, translate=None, id_field='COMID'):
+def write_to_file(output_dict, outfile_name, id_field='COMID'):
 
     def is_num(x):
         try:
@@ -327,7 +368,6 @@ def write_to_file(output_dict, outfile_name, translate=None, id_field='COMID'):
         writer.writeheader()
         for comid, row in output_dict.iteritems():
             row = dict(zip(format_keys(row.keys()), row.values()))
-            comid = translate.get(comid, -comid) if translate else comid
             row[id_field] = comid
             if row:
                 writer.writerow(row)
---
+++ ca3t_clone.py
@@ -0,0 +1,31 @@
+from array_tools import *
+import os
+import time
+
+def ca3t(nhd_dir, allocation_file, output_dir, overwrite):
+    nhd_files = get_nhd(nhd_dir)
+    basename = os.path.basename(allocation_file)
+    for region, path in sorted(nhd_files.iteritems()):
+        region_start = time.time()
+        print "\tProcessing region {}..."format(region)
+        output_basename = os.path.join(output_dir, "{}_{}.csv".format(basename, region))
+        if not os.path.exists(output_basename) or overwrite:
+            catchment_raster = Raster(os.path.join(path, "NHDPlusCatchment", "cat"))
+            allocation_array = allocate(Raster(allocation_file), catchment_raster, tile=1000000)
+            if accumulate:
+                region_hierarchy = Hierarchy(path, region)
+                allocation_array = accumulate(allocation_array, region_hierarchy)
+            allocation_dict = make_histogram(allocation_array)
+            write_to_file(allocation_dict, output_basename)
+            print "\tRegion complete in {} seconds".format(int(time.time() - region_start))
+        else:
+            print "\t...already processed"
+
+def main():
+    nhd_dir = r"G:\NationalData\NHDPlusV2"
+    attribution_file = r"G:\NationalData\CDL\CDL_finals\cdlx_10"
+    output_dir = r"G:\CA3T\TestOutput"
+    overwrite = True
+    ca3t(nhd_dir, attribution_file, output_dir, overwrite)
+
+main()
